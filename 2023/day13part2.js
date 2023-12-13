481a9ff4a54ad84233d47efbf52ec8fc142589ae
hiawui <hiawui@gmail.com>
2023-12-13 21:00:30 +0800

    Create day13part2.js

---
+++ 2023/day13part2.js
@@ -0,0 +1,85 @@
+input = `#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#
+`;
+
+patterns = [];
+
+input
+  .split("\n\n")
+  .filter((v) => v)
+  .forEach((p) => {
+    const map = {};
+    let maxI = 0;
+    let maxJ = 0;
+    p.split("\n")
+      .filter((v) => v)
+      .forEach((r, i) => {
+        maxI++;
+        maxJ = r.length;
+        for (let j = 0; j < r.length; ++j) {
+          map[`${i},${j}`] = r.charAt(j);
+        }
+      });
+    patterns.push({
+      map,
+      i: maxI,
+      j: maxJ,
+    });
+  });
+
+res = 0;
+patterns.forEach((p, pi) => {
+  // row
+  for (let i = 0; i < p.i - 1; ++i) {
+    let diff = 0;
+    for (let j = 0; j < p.j; ++j) {
+      for (let k = 0; i - k >= 0 && i + k + 1 < p.i; ++k) {
+        if (p.map[`${i - k},${j}`] !== p.map[`${i + k + 1},${j}`]) {
+          diff++;
+        }
+      }
+      if (diff > 1) {
+        break;
+      }
+    }
+    if (diff === 1) {
+      //   console.log(`p: ${pi}, row: ${i}`);
+      res += (i + 1) * 100;
+      break;
+    }
+  }
+
+  // col
+  for (let j = 0; j < p.j - 1; ++j) {
+    let diff = 0;
+    for (let i = 0; i < p.i; ++i) {
+      for (let k = 0; j - k >= 0 && j + k + 1 < p.j; ++k) {
+        if (p.map[`${i},${j - k}`] !== p.map[`${i},${j + k + 1}`]) {
+          diff++;
+        }
+      }
+      if (diff > 1) {
+        break;
+      }
+    }
+    if (diff === 1) {
+      //   console.log(`p: ${pi}, col: ${j}`);
+      res += j + 1;
+      break;
+    }
+  }
+});
+console.log(res);
