64c418d3a2f4e5da4abe16f1e6d2382d6967b550
hiawui <hiawui@gmail.com>
2024-01-11 15:53:02 +0800

    Update day24part1.js

--- 2023/day24part1.js
+++ 2023/day24part1.js
@@ -17,80 +17,43 @@ input
     obj.vel = v.split(/ *, */).map((v) => Number(v));
   });
 
-// v0 * t0 - v1 * t1 == p1 - p0
-let resolve2 = (hail0, hail1, d) => {
-  const matrix = [];
-  for (let i = 0; i < d; ++i) {
-    matrix.push([hail0.vel[i], -hail1.vel[i], hail1.pos[i] - hail0.pos[i]]);
-  }
-
-  // 高斯消元
-  let row = 0;
-  let col = 0;
-  do {
-    let maxRow = row;
-    for (let i = row + 1; i < matrix.length; ++i) {
-      if (Math.abs(matrix[row][col]) > Math.abs(matrix[maxRow][col])) {
-        maxRow = row;
-      }
-    }
-    if (matrix[maxRow][col] === 0) {
-      col++;
-      continue;
-    }
-
-    const tmp = matrix[row];
-    matrix[row] = matrix[maxRow];
-    matrix[maxRow] = tmp;
-
-    const div = matrix[row][col];
-    matrix[row] = matrix[row].map((v) => v / div);
-    for (let i = row + 1; i < matrix.length; ++i) {
-      const m = matrix[i][col];
-      matrix[i] = matrix[i].map((v, j) => v - m * matrix[row][j]);
-    }
-
-    row++;
-    col++;
-  } while (row < matrix.length && col < 2);
-
-  row = matrix.length - 1;
-  col = 1;
-  do {
-    if (matrix[row][col] === 0) {
-      row--;
-      continue;
-    }
-    for (let i = row - 1; i >= 0; --i) {
-      const m = matrix[i][col];
-      matrix[i] = matrix[i].map((v, j) => v - m * matrix[row][j]);
-    }
-    row--;
-    col--;
-  } while (row > 1 && col > 0);
-  if (matrix[1][1] === 0) {
-    return [-Infinity, -Infinity];
-  }
-  return [matrix[0][2], matrix[1][2]];
+/**
+ * v0 * t0 - v1 * t1 == p1 - p0
+ *
+ * vx0 * t0 - vx1 * t1 = x1 - x0
+ * vy0 * t0 - vy1 * t1 = y1 - y0
+ * t1 = (vx0 * t0 - x1 + x0) / vx1
+ * vy0 * t0 - vy1 * (vx0 * t0 - x1 + x0) / vx1 = y1 - y0
+ * (vy0 - vy1 * vx0 / vx1) * t0 + vy1 * x1 / vx1 - vy1 * x0 / vx1 = y1 - y0
+ * t0 = (y1 - y0 - vy1 * x1 / vx1 + vy1 * x0 / vx1) / (vy0 - vy1 * vx0 / vx1)
+ */
+let resolve2 = (hail0, hail1) => {
+  const t0 =
+    (hail1.pos[1] -
+      hail0.pos[1] -
+      (hail1.vel[1] * hail1.pos[0]) / hail1.vel[0] +
+      (hail1.vel[1] * hail0.pos[0]) / hail1.vel[0]) /
+    (hail0.vel[1] - (hail1.vel[1] * hail0.vel[0]) / hail1.vel[0]);
+  const t1 = (hail0.vel[0] * t0 - hail1.pos[0] + hail0.pos[0]) / hail1.vel[0];
+  return [t0, t1];
 };
 
-let dim = 2;
 let cnt = 0;
 for (let i = 0; i < hails.length; ++i) {
   for (let j = i + 1; j < hails.length; ++j) {
-    const [ti, tj] = resolve2(hails[i], hails[j], dim);
+    const [ti, tj] = resolve2(hails[i], hails[j]);
     if (ti < 0 || tj < 0) {
       continue;
     }
     const point = [];
-    for (let k = 0; k < dim; ++k) {
+    for (let k = 0; k < 2; ++k) {
       const e = hails[i].pos[k] + hails[i].vel[k] * ti;
       if (e < range[0] || e > range[1]) {
         break;
       }
       point.push(e);
     }
-    if (point.length === dim) {
+    if (point.length === 2) {
       cnt++;
     }
   }
