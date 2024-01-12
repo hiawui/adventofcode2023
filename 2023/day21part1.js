5532dabdeeb3adde779df731ddc73cdf2ea4e7f3
hiawui <hiawui@gmail.com>
2024-01-12 10:52:25 +0800

    Update day21part1.js

--- 2023/day21part1.js
+++ 2023/day21part1.js
@@ -12,12 +12,16 @@ input = `
 ...........
 `;
 
+let rowCnt = 0;
+let colCnt = 0;
 let map = {};
 let start = null;
 input
   .split("\n")
   .filter((v) => v)
   .forEach((r, i) => {
+    rowCnt++;
+    colCnt = r.length;
     for (let j = 0; j < r.length; ++j) {
       map[`${i},${j}`] = {
         i,
@@ -30,38 +34,40 @@ input
     }
   });
 
-let cache = {};
-let go = (i, j, steps) => {
-  const goK = `${i},${j},${steps}`;
-  if (goK in cache) {
-    return cache[goK];
+let getTile = (i, j) => {
+  if (`${i},${j}` in map) {
+    return map[`${i},${j}`];
   }
-  if (steps === 0) {
-    return { [`${i},${j}`]: 1 };
-  }
-  const pos = {};
-  [
-    [-1, 0],
-    [1, 0],
-    [0, -1],
-    [0, 1],
-  ].forEach(([di, dj]) => {
-    const [ni, nj] = [i + di, j + dj];
-    const nk = `${ni},${nj}`;
-    if (!(nk in map) || map[nk].type === "#") {
-      return;
-    }
-    pos[nk] = { i: ni, j: nj };
-  });
+  return null;
+};
+
+let go1 = (pos) => {
   const res = {};
-  Object.values(pos).forEach(({ i, j }) => {
-    Object.keys(go(i, j, steps - 1)).forEach((k) => {
-      res[k] = 1;
+  for (const [i, j] of Object.values(pos)) {
+    [
+      [-1, 0],
+      [0, 1],
+      [1, 0],
+      [0, -1],
+    ].forEach(([di, dj]) => {
+      const [ni, nj] = [i + di, j + dj];
+      const nTile = getTile(ni, nj);
+      if (nTile === null || nTile.type === "#") {
+        return;
+      }
+      const nk = `${ni},${nj}`;
+      if (nk in res) {
+        return;
+      }
+      res[nk] = [ni, nj];
     });
-  });
-  cache[goK] = res;
+  }
   return res;
 };
 
-let res = go(start[0], start[1], 64);
-console.log(Object.keys(res).length);
+let step = 64;
+let pos = { [`${start[0]},${start[1]}`]: start };
+while (step-- > 0) {
+  pos = go1(pos);
+}
+console.log(Object.keys(pos).length);
